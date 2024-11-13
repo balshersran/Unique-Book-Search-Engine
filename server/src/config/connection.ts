7837aa3441bac2ddf97babe824940b197f6e2845
Balsher Sran <balshersran@Balshers-MacBook-Pro.local>
2024-11-12 23:21:50 -0800

    was having issues connecting to DB, bypassed it on the connections page, delete seeds for it was never seeding

--- server/src/config/connection.ts
+++ server/src/config/connection.ts
@@ -2,7 +2,8 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 dotenv.config();
 
-const MONGODB_URI = process.env.MONGODB_URI || '';
+const MONGODB_URI = 'mongodb://127.0.0.1:27017/googlebooks' || '';
+console.log(MONGODB_URI);
 
 const db = async (): Promise<typeof mongoose.connection> => {
     try {
