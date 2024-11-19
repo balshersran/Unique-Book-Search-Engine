f469730f0074f6163c648c476b667ead8be5a492
Balsher Sran <balshersran@Balshers-MacBook-Pro.local>
2024-11-19 00:06:32 -0800

    fixed directory paths

--- server/src/server.ts
+++ server/src/server.ts
@@ -33,10 +33,10 @@ const startApolloServer = async () => {
   ));
 
   if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/build')));
+    app.use(express.static(path.join(__dirname, '../../client/build')));
 
     app.get('*', (_req: Request, res: Response) => {
-      res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+      res.sendFile(path.join(__dirname, '../../client/dist/index.html'));
     });
   }
 
