22c7935325d89cfe23278bdb2e9d4e1836c5f784
Balsher Sran <balshersran@Balshers-MacBook-Pro.local>
2024-11-18 16:05:51 -0800

    got it working and running

--- client/src/pages/SearchBooks.tsx
+++ client/src/pages/SearchBooks.tsx
@@ -50,7 +50,7 @@ const SearchBooks = () => {
         description: book.volumeInfo.description,
         image: book.volumeInfo.imageLinks?.thumbnail || "",
       }));
-
+      console.log(bookData);
       setSearchedBooks(bookData);
       setSearchInput("");
     } catch (err) {
