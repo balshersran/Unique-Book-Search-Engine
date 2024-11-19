b48535c6025ca2833c90cd2de39edc547c3eec85
Balsher Sran <balshersran@Balshers-MacBook-Pro.local>
2024-11-19 09:39:31 -0800

    fixed imports again

--- server/src/server.ts
+++ server/src/server.ts
@@ -9,6 +9,13 @@ import { expressMiddleware } from '@apollo/server/express4';
 import { typeDefs, resolvers } from './schemas/index.js';
 import { authenticateToken } from './services/auth.js';
 
+import {fileURLToPath} from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+
+// 👇️ "/home/john/Desktop/javascript"
+const __dirname = path.dirname(__filename);
+
 const PORT = process.env.PORT || 3001;
 
 const server = new ApolloServer({
