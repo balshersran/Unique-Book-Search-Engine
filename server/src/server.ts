97bd41419cd91ae194dbdf86b3cadc81c681a708
Balsher Sran <balshersran@Balshers-MacBook-Pro.local>
2024-11-18 16:36:44 -0800

    corrected imports

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,5 +1,6 @@
 import express from 'express';
-import path from 'node:path';
+import path, { dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
 import db from './config/connection.js';
 // import routes from './routes/index.js';
 import type { Request, Response } from 'express';
@@ -16,6 +17,7 @@ const server = new ApolloServer({
 });
 
 const app = express();
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const startApolloServer = async () => {
   await server.start();
